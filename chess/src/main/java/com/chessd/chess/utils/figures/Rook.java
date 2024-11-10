f45901968b07bc037ed09fe9097d0a6b176b0afe
DawidSzoka1 <oficer130@gmail.com>
2024-11-10 18:39:51 +0100

    rook

---
+++ chess/src/main/java/com/chessd/chess/utils/figures/Rook.java
@@ -0,0 +1,7 @@
+package com.chessd.chess.utils.figures;
+
+public class Rook extends Figure {
+    public Rook(int x, int y, boolean white, boolean active) {
+        super(x, y, white, active);
+    }
+}
