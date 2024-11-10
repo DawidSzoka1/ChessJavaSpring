b1776a9d1359b03e4b8e94f439ece9f9e6ecbad5
DawidSzoka1 <oficer130@gmail.com>
2024-11-10 17:38:17 +0100

    user details mapping with empty html

--- chess/src/main/java/com/chessd/chess/controller/UserController.java
+++ chess/src/main/java/com/chessd/chess/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.chessd.chess.controller;
 
+import com.chessd.chess.entity.User;
 import com.chessd.chess.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/users")
@@ -22,5 +26,15 @@ public class UserController {
     public String all(Model model) {
         model.addAttribute("users", userService.findAll());
         return "users/all";
+    }
+
+    @GetMapping("/{id}")
+    public String details(Model model, @PathVariable int id) throws Exception {
+        Optional<User> user = userService.findById(id);
+        if (user.isEmpty()) {
+            throw new Exception("user not found");
+        }
+        model.addAttribute("user", user.get());
+        return "users/details";
     }
 }
