01163f43802499a534066c5ffd36f8599a790698
DawidSzoka1 <oficer130@gmail.com>
2024-11-18 19:20:17 +0100

    cleaning some mess

--- chess/src/main/java/com/chessd/chess/config/UserSecurityConfig.java
+++ chess/src/main/java/com/chessd/chess/config/UserSecurityConfig.java
@@ -1,17 +1,12 @@
 package com.chessd.chess.config;
 
 import com.chessd.chess.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -41,14 +36,12 @@ public class UserSecurityConfig {
                         configurer
                                 .requestMatchers("/").permitAll()
                                 .requestMatchers("/showMyLoginPage").anonymous()
-                                .requestMatchers("/leaders/**").hasRole("MANAGER")
-                                .requestMatchers("/users/**").hasRole("ADMIN")
                                 .requestMatchers("/game/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .formLogin(form ->
                         form
-                                .loginPage("/login")
+                                .loginPage("/showMyLoginPage")
                                 .loginProcessingUrl("/authenticateTheUser")
                                 .permitAll()
                 )
@@ -62,21 +55,3 @@ public class UserSecurityConfig {
     }
 
 }
-
-//    @Bean
-//    public UserDetailsManager userDetailsManager(DataSource dataSource) {
-//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-//
-//        jdbcUserDetailsManager.setUsersByUsernameQuery(
-//                "select id, username, password, active from users where username=?");
-//
-//        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
-//                "select username_ref, role from roles where username_ref=?"
-//
-//        );
-//
-//        return jdbcUserDetailsManager;
-//
-//    }
-
-
