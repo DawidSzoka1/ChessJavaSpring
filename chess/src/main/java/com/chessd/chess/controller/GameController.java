c6af24e5d93885dd5151a6a3e09a2004dce9b7e9
DawidSzoka1 <oficer130@gmail.com>
2024-11-27 17:15:50 +0100

    cleaning GameController

--- chess/src/main/java/com/chessd/chess/controller/GameController.java
+++ chess/src/main/java/com/chessd/chess/controller/GameController.java
@@ -1,7 +1,5 @@
 package com.chessd.chess.controller;
 
-import com.chessd.chess.utils.Board;
-import com.chessd.chess.utils.Player;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/game")
 public class GameController {
 
-
-
     @GetMapping("/classic")
     public String classic(Model model) {
-        Player whitePlayer = new Player();
-        Player blackPlayer = new Player();
-        Board board = new Board(whitePlayer, blackPlayer, "on_going");
-        board.startGame();
-        model.addAttribute("board", board);
-        model.addAttribute("player", whitePlayer);
+
         return "game/classic-board";
     }
 
