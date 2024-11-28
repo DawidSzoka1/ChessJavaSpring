0e5dd43456fc73dac08b787d0541736ee5d12d4e
DawidSzoka1 <oficer130@gmail.com>
2024-11-28 14:31:20 +0100

    Figure repository finished

--- chess/src/main/java/com/chessd/chess/repository/gameRepository/FigureDao.java
+++ chess/src/main/java/com/chessd/chess/repository/gameRepository/FigureDao.java
@@ -5,9 +5,8 @@ import com.chessd.chess.entity.gameEntity.Figure;
 import java.util.List;
 
 public interface FigureDao {
-    Figure getFigureByPosition(String position);
-    List<Figure> getAllFigures();
-    Figure getFigureByName(String figureName);
+    Figure getFigureByPosition(String position, String gameId);
+    List<Figure> getAllFigures(String gameId);
     void save(Figure figure);
     void delete(Figure figure);
     void update(Figure figure);
--- chess/src/main/java/com/chessd/chess/repository/gameRepository/FigureDaoImpl.java
+++ chess/src/main/java/com/chessd/chess/repository/gameRepository/FigureDaoImpl.java
@@ -3,54 +3,66 @@ package com.chessd.chess.repository.gameRepository;
 import com.chessd.chess.entity.User;
 import com.chessd.chess.entity.gameEntity.Figure;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public class FigureDaoImpl implements FigureDao {
     private EntityManager entityManager;
+    private GameDao gameDao;
 
     @Autowired
-    public FigureDaoImpl(EntityManager entityManager) {
+    public FigureDaoImpl(EntityManager entityManager, GameDao gameDao) {
         this.entityManager = entityManager;
+        this.gameDao = gameDao;
     }
 
 
     @Override
-    public Figure getFigureByPosition(String position) {
-        return null;
+    public Figure getFigureByPosition(String position, String gameId) {
+        TypedQuery<Figure> query = entityManager.createQuery(
+                "FROM Figure where position=:position and game=:game",
+                Figure.class
+        );
+        query.setParameter("position", position);
+        query.setParameter("game", gameDao.getGameById(gameId));
+        return query.getSingleResult();
     }
 
     @Override
-    public List<Figure> getAllFigures() {
-        return List.of();
+    public List<Figure> getAllFigures(String gameId) {
+        TypedQuery<Figure> query = entityManager.createQuery(
+                "FROM Figure where game=:game",
+                Figure.class
+        );
+        query.setParameter("game", gameDao.getGameById(gameId));
+        return query.getResultList();
     }
 
-
     @Override
-    public Figure getFigureByName(String figureName) {
-        return null;
-    }
-
-    @Override
+    @Transactional
     public void save(Figure figure) {
-
+        entityManager.persist(figure);
     }
 
     @Override
+    @Transactional
     public void delete(Figure figure) {
-
+        entityManager.remove(figure);
     }
 
     @Override
+    @Transactional
     public void update(Figure figure) {
-
+        entityManager.merge(figure);
     }
 
     @Override
     public void moveToPosition(Figure figure, String position) {
-
+        figure.setPosition(position);
     }
 }
