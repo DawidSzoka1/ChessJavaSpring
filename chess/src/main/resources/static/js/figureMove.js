fbf4bbb293f8582cc1d844027e0ff2a4e3407224
DawidSzoka1 <oficer130@gmail.com>
2024-12-04 18:58:08 +0100

    some javascript to make pawns move

--- chess/src/main/resources/static/js/figureMove.js
+++ chess/src/main/resources/static/js/figureMove.js
@@ -1,9 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
     const squares = document.querySelectorAll(".square-content");
+    const pieces = document.querySelectorAll(".piece");
+    const container = document.querySelector(".container")
+    const containerRect = container.getBoundingClientRect()
 
-    squares.forEach(square => {
-        square.addEventListener("click", event => {
-            console.log(`Kliknieto pole: ${event.target.id}`)
+    let selectedPiece
+    pieces.forEach(piece => {
+        piece.addEventListener("mousedown", (e) => {
+            selectedPiece = piece;
         })
     })
+
+
+    container.addEventListener("mousemove", (e) => {
+        if(selectedPiece){
+            const mouseX = e.clientX - containerRect.x;
+            const mouseY = e.clientY - containerRect.y;
+            selectedPiece.style.left = mouseX + 'px'
+            selectedPiece.style.top = mouseY + 'px'
+        }
+    })
+    container.addEventListener("mouseup", (e) =>{
+        e.preventDefault()
+        if (selectedPiece){
+            selectedPiece = null;
+        }
+    })
+
 })
