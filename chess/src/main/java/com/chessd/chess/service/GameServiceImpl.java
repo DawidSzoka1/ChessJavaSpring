eef203773ae3eab0e50b38ef8d2f6a538b0d676d
DawidSzoka1 <oficer130@gmail.com>
2024-12-08 20:38:58 +0100

    refactoring to use dry

--- chess/src/main/java/com/chessd/chess/service/GameServiceImpl.java
+++ chess/src/main/java/com/chessd/chess/service/GameServiceImpl.java
@@ -9,13 +9,13 @@ import com.chessd.chess.utils.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import java.util.*;
 
 @Service
 public class GameServiceImpl implements GameService {
     private GameDao gameDao;
     private MoveDao moveDao;
+    private final String[] figuresName = {"rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"};
 
     @Autowired
     public GameServiceImpl(GameDao gameDao, MoveDao moveDao) {
@@ -23,39 +23,24 @@ public class GameServiceImpl implements GameService {
         this.moveDao = moveDao;
     }
 
-    //filling back row without king and queen for white and black
+    //method to create correct Figure
+    public Figure putFigure(String figureName, String color, String position) {
+        return switch (figureName) {
+            case "rook" -> new Rook(color, position);
+            case "bishop" -> new Bishop(color, position);
+            case "knight" -> new Knight(color, position);
+            case "king" -> new King(color);
+            case "queen" -> new Queen(color);
+            default -> throw new IllegalArgumentException("Unknown figure name " + figureName);
+        };
+    }
+
+    //filling back row for white and black
     public void fileBackRow(Game game) {
-        for (int i = 0; i < 2; i++) {
-            int[] knightCol = {1, 6};
-            int[] bishopCol = {2, 5};
-            //putting rooks on a1, h1 and a8, h8
-            game.placeFigure(
-                    0,
-                    i * 7,
-                    new Rook("W", Column.fromIndex(i * 7).get() + "1"));
-            game.placeFigure(
-                    7, i * 7,
-                    new Rook("B", Column.fromIndex(i * 7).get() + "8"));
+        for (int i = 0; i < 8; i++) {
+            game.placeFigure(0, i, putFigure(this.figuresName[i], "W", Column.fromIndex(i) + "1"));
+            game.placeFigure(7, i, putFigure(this.figuresName[i], "B", Column.fromIndex(i) + "7"));
 
-            // putting knights on b1, g1 and b8, g8
-            game.placeFigure(
-                    0,
-                    knightCol[i],
-                    new Knight("W", Column.fromIndex(knightCol[i]).get() + "1"));
-            game.placeFigure(
-                    7,
-                    knightCol[i],
-                    new Knight("B", Column.fromIndex(knightCol[i]).get() + "8"));
-
-            //putting bishops on c1, f1 and c8, f8
-            game.placeFigure(
-                    0,
-                    bishopCol[i],
-                    new Bishop("W", Column.fromIndex(bishopCol[i]).get() + "1"));
-            game.placeFigure(
-                    7,
-                    bishopCol[i],
-                    new Bishop("B", Column.fromIndex(bishopCol[i]).get() + "1"));
         }
     }
 
@@ -70,13 +55,6 @@ public class GameServiceImpl implements GameService {
             game.placeFigure(6, i, new Pawn("B", Column.fromIndex(i).get() + "7"));
         }
         fileBackRow(game);
-        //putting kings on e1 and e8
-        game.placeFigure(0, 4, new King("W"));
-        game.placeFigure(7, 4, new King("B"));
-
-        //putting queens on d1 and d8
-        game.placeFigure(0, 3, new Queen("W"));//White queen on d1
-        game.placeFigure(7, 3, new Queen("B"));// Black queen on d8
     }
 
     @Override
