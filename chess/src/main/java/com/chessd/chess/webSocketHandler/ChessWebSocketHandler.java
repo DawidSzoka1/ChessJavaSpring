1255b929c47dc7b99bfd1debbb8237d3ff20f927
DawidSzoka1 <oficer130@gmail.com>
2024-12-23 02:07:52 +0100

    Using GameHandleTextMessage class in every message from js

--- chess/src/main/java/com/chessd/chess/webSocketHandler/ChessWebSocketHandler.java
+++ chess/src/main/java/com/chessd/chess/webSocketHandler/ChessWebSocketHandler.java
@@ -1,42 +1,42 @@
 package com.chessd.chess.webSocketHandler;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import org.springframework.scheduling.annotation.Scheduled;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-import java.io.IOException;
 
 public class ChessWebSocketHandler extends TextWebSocketHandler {
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+    public void afterConnectionEstablished(@NotNull WebSocketSession session) throws Exception {
         session.sendMessage(new TextMessage("Game starts now"));
     }
 
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+    protected void handleTextMessage(@NotNull WebSocketSession session, @NotNull TextMessage message) throws Exception {
         String payload = message.getPayload();
+        ObjectMapper objectMapper = new ObjectMapper();
+        GameHandleTextMessage gameHandleTextMessage =
+                objectMapper.readValue(payload, GameHandleTextMessage.class);
+        session.sendMessage(new TextMessage(gameHandleTextMessage.handleMessage()));
+        System.out.println(gameHandleTextMessage);
 
-        if(payload.equals("PING")){
-            session.sendMessage(new TextMessage("PONG"));
-        }else{
-            System.out.println(payload);
-        }
     }
 
     @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+    public void afterConnectionClosed(@NotNull WebSocketSession session, @NotNull CloseStatus status) throws Exception {
         System.out.println("Connection closed: " + status.getReason());
     }
 
-    @Scheduled(fixedRate= 30000)
-    public void sendPing(WebSocketSession session) throws IOException {
-        if(session.isOpen()){
-            session.sendMessage(new TextMessage("PONG"));
-        }else{
-            System.out.println("Some error occurred");
-        }
-
-    }
+//    @Scheduled(fixedRate= 30000)
+//    public void sendPing(WebSocketSession session) throws IOException {
+//        if(session.isOpen()){
+//            session.sendMessage(new TextMessage("PONG"));
+//        }else{
+//            System.out.println("Some error occurred");
+//        }
+//
+//    }
 }
--- chess/src/main/java/com/chessd/chess/webSocketHandler/GameHandleTextMessage.java
+++ chess/src/main/java/com/chessd/chess/webSocketHandler/GameHandleTextMessage.java
@@ -1,10 +1,34 @@
 package com.chessd.chess.webSocketHandler;
 
+import com.chessd.chess.repository.gameRepository.GameDaoImpl;
+import com.chessd.chess.repository.gameRepository.MoveDaoImpl;
+import com.chessd.chess.service.GameService;
+import com.chessd.chess.service.GameServiceImpl;
+
 public class GameHandleTextMessage {
     private String message;
+    private String messageType;
     private String gameId;
-    public GameHandleTextMessage(String message, String gameId){
+    public GameHandleTextMessage(){}
+    public GameHandleTextMessage(String message, String gameId, String messageType){
         this.message = message;
+        this.gameId = gameId;
+        this.messageType = messageType;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+
+    public void setGameId(String gameId) {
         this.gameId = gameId;
     }
 
@@ -12,8 +36,18 @@ public class GameHandleTextMessage {
         this.message = message;
     }
 
-    public void handleMessageMove(){
+    public String handleMessageMove(){
 
+        return "Success";
+    }
+    public String pongMessage(){
+        return "PONG";
+    }
+    public String handleMessage(){
+        return switch (messageType){
+            case "move" -> handleMessageMove();
+            default -> pongMessage();
+        };
     }
 
     public String getMessage() {
@@ -26,6 +60,6 @@ public class GameHandleTextMessage {
 
     @Override
     public String toString() {
-        return "message='" + message;
+        return "message=" + message + " " + messageType;
     }
 }
