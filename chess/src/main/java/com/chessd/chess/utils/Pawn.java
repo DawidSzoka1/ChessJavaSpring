d64f14f47115c802f6d5049bc5a5a1cbea85ce90
DawidSzoka1 <oficer130@gmail.com>
2025-01-08 19:47:29 +0100

    fixing some errors with pawns double move

--- chess/src/main/java/com/chessd/chess/utils/Pawn.java
+++ chess/src/main/java/com/chessd/chess/utils/Pawn.java
@@ -8,21 +8,12 @@ import java.util.List;
 
 public class Pawn extends Figure {
     private static final Logger log = LoggerFactory.getLogger(Pawn.class);
-    private boolean first = true;
     private int direction;
 
     public Pawn(String color, String position) {
         super("pawn", color, position);
         direction = this.getColor().equals("W") ? 1 : -1;
         this.setMoves(availableMoves());
-    }
-
-    public boolean isFirst() {
-        return first;
-    }
-
-    public void setFirst(boolean first) {
-        this.first = first;
     }
 
     public int getDirection() {
@@ -38,10 +29,10 @@ public class Pawn extends Figure {
         List<String> moves = new ArrayList<>();
         int row = Integer.parseInt(String.valueOf(this.getPosition().charAt(1)));
         moves.add(String.valueOf(this.getPosition().charAt(0)) + (row + this.direction));
-        if (this.first) {
+        if ((row == 2 && this.getColor().equals("W")) ||
+                (row == 7 && this.getColor().equals("B"))) {
             moves.add(String.valueOf(this.getPosition().charAt(0)) + (row + this.direction * 2));
         }
-        System.out.println(moves);
         return moves;
     }
 }
