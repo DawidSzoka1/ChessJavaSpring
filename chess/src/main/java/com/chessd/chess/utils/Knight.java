f473c798c814dbd5c9b13577af5133e71a8b79ec
DawidSzoka1 <oficer130@gmail.com>
2025-01-08 19:46:58 +0100

    availableMoves for knight right now without taking others figures

--- chess/src/main/java/com/chessd/chess/utils/Knight.java
+++ chess/src/main/java/com/chessd/chess/utils/Knight.java
@@ -1,5 +1,6 @@
 package com.chessd.chess.utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -8,9 +9,20 @@ public class Knight extends Figure {
         super("knight", color, position);
     }
 
-    //TODO: moves
     @Override
     public List<String> availableMoves() {
-        return List.of();
+        ArrayList<String> moves = new ArrayList<>();
+        int currentRow = this.getPosition().charAt(1) - '0';
+        char currentCol = this.getPosition().charAt(0);
+        int[] rowPossibilities = {2, 2, -2, -2, 1, 1, -1, -1};
+        int[] colPossibilities = {1, -1, 1, -1, 2, -2, 2, -2};
+        for (int i = 0; i < rowPossibilities.length; i++) {
+            int newRow = currentRow + rowPossibilities[i];
+            char newCol = (char) (currentCol + colPossibilities[i]);
+            if (newRow >= 1 && newRow <= 8 && newCol >= 'a' && newCol <= 'h') {
+                moves.add("" + newCol + newRow);
+            }
+        }
+        return moves;
     }
 }
