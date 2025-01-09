9dc57ae0f448b37a22c70ff6f0012588933271b0
DawidSzoka1 <oficer130@gmail.com>
2025-01-09 17:33:09 +0100

    changing the way of doing moves for rook

--- chess/src/main/java/com/chessd/chess/utils/Rook.java
+++ chess/src/main/java/com/chessd/chess/utils/Rook.java
@@ -13,27 +13,32 @@ public class Rook extends Figure {
         ArrayList<String> moves = new ArrayList<>();
         int startRow = this.getPosition().charAt(1) - '0';
         char startCol = this.getPosition().charAt(0);
-        int[] newRow = new int[4];
-        char[] newCol = new char[4];
-        for (int i = 0; i < 8; i++) {
-            //moving horizontally
-            for (int hor = 0; hor < 2; hor++) {
-                newRow[hor] = startRow;
-                newCol[hor] = (char) (startCol + ((int) Math.pow(-1, hor) * i));
-                if (this.validPostion(newRow[hor], newCol[hor])) {
-                    moves.add("" + newCol[hor] + newRow[hor]);
+        //moving horizontally
+        for (int hor = -1; hor <= 1; hor += 2) {
+            char col = startCol;
+            while (true) {
+                col += hor;
+                if (this.validPostion(startRow, col)) {
+                    moves.add("" + col + startRow);
+                } else {
+                    break;
                 }
             }
-            //moving vertically
-            for (int ver = 2; ver < 4; ver++) {
-                newRow[ver] = startRow + ((int) Math.pow(-1, ver) * i);
-                newCol[ver] = startCol;
-                if (this.validPostion(newRow[ver], newCol[ver])) {
-                    moves.add("" + newCol[ver] + newRow[ver]);
+        }
+        //moving vertically
+        for (int ver = -1; ver <= 1; ver += 2) {
+            int row = startRow;
+            while (true) {
+                row += ver;
+                if (this.validPostion(row, startCol)) {
+                    moves.add("" + startCol + row);
+                } else {
+                    break;
                 }
             }
         }
         return moves;
     }
-
 }
+
+
