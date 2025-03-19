41687f93d3ee081128f69453197a1fe89e0c9eea
DawidSzoka1 <oficer130@gmail.com>
2025-03-19 21:48:02 +0100

    user details custom class and update controller with UpdateUser for web

---
+++ chess/src/main/java/com/chessd/chess/controller/user/UpdateController.java
@@ -0,0 +1,77 @@
+package com.chessd.chess.controller.user;
+
+import com.chessd.chess.entity.User;
+import com.chessd.chess.service.UserService;
+import com.chessd.chess.user.web.UpdateUser;
+import com.chessd.chess.utils.SessionHelper;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/users")
+public class UpdateController {
+    private final UserService userService;
+    private final SessionHelper sessionHelper;
+
+    @Autowired
+    public UpdateController(UserService userService, SessionHelper sessionHelper) {
+        this.userService = userService;
+        this.sessionHelper = sessionHelper;
+    }
+
+    private String validUpdateUser(UpdateUser updateUser, User user) {
+        User emailTaken = userService.findByEmail(updateUser.getEmail());
+        User userNameTaken = userService.findByUserName(updateUser.getUserName());
+        if (emailTaken != null && emailTaken.getId() != user.getId()) {
+            return "Email is taken";
+        }
+        if (userNameTaken != null && userNameTaken.getId() != user.getId()) {
+            return "Username is taken";
+        }
+        return "";
+    }
+
+
+    @GetMapping("/update")
+    public String updateInfo(Model model, Principal principal) {
+        User user = userService.findByUserName(principal.getName());
+        model.addAttribute("user", user);
+        model.addAttribute("error", false);
+        return "users/update";
+    }
+
+    @PostMapping("/update")
+    public String updateProcess(
+            @Valid @ModelAttribute("user") UpdateUser updateUser,
+            BindingResult bindingResult,
+            HttpSession session,
+            Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("error", bindingResult.getAllErrors());
+            return "users/update";
+        }
+        User user = sessionHelper.getUserFromPrincipal(session);
+        String check = this.validUpdateUser(updateUser, user);
+        if (!check.isEmpty()) {
+            model.addAttribute("error", check);
+            return "redirect:users/update";
+        }
+        if (!updateUser.getUserName().equals(user.getUserName())) {
+            sessionHelper.updateUserInSession(updateUser, session);
+        }
+        userService.update(user.getId(), updateUser);
+
+        model.addAttribute("success", "Successfully update info");
+        return "users/profile";
+    }
+}
---
+++ chess/src/main/java/com/chessd/chess/user/CustomUserDetails.java
@@ -0,0 +1,37 @@
+package com.chessd.chess.user;
+
+import com.chessd.chess.entity.Role;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private String username;
+    private String password;
+    private boolean enabled;
+    private Collection<Role> roles;
+
+    public CustomUserDetails(String username, String password, boolean enabled, Collection<Role> roles) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        for(Role role: this.roles){
+            SimpleGrantedAuthority tempAuthority = new SimpleGrantedAuthority(role.getName());
+            authorities.add(tempAuthority);
+        }
+        return authorities;
+    }
+
+}
