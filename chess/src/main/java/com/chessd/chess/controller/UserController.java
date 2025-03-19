419b77571162757515d25e20d975b2e2e9d9ef54
DawidSzoka1 <oficer130@gmail.com>
2025-03-19 08:55:41 +0100

    post mapping for update not tested yet and starting to create form and in profile correct value for country and gender

--- chess/src/main/java/com/chessd/chess/controller/UserController.java
+++ chess/src/main/java/com/chessd/chess/controller/UserController.java
@@ -2,16 +2,21 @@ package com.chessd.chess.controller;
 
 import com.chessd.chess.entity.User;
 import com.chessd.chess.service.UserService;
+import com.chessd.chess.web.WebUser;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/users")
@@ -35,6 +40,30 @@ public class UserController {
         User user = userService.findByUserName(principal.getName());
         model.addAttribute("user", user);
         return "users/update";
+    }
+
+    @PostMapping("/update")
+    public String updateProcess(
+            @Valid @ModelAttribute("user") WebUser webUser,
+            BindingResult bindingResult,
+            HttpSession session,
+            Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("errors", bindingResult.getAllErrors());
+            return "users/update";
+        }
+        Object obj = session.getAttribute("user");
+        assert obj instanceof WebUser;
+        WebUser user = (WebUser) obj;
+        if(!webUser.equals(user)){
+            model.addAttribute("error",
+                    "Try to change values for different user");
+            return "users/update";
+        }
+
+        userService.update(webUser);
+        model.addAttribute("success", "Successfully update info");
+        return "redirect:/users/profile";
     }
 
     @GetMapping("/rankings")
