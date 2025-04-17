65de57b285bf5efd6f79fb4094631673fa37be4f
DawidSzoka1 <oficer130@gmail.com>
2025-04-17 17:47:45 +0200

    testing view for game plus css from chessboard in global

--- chess/src/main/java/com/chessd/chess/game/controller/GameController.java
+++ chess/src/main/java/com/chessd/chess/game/controller/GameController.java
@@ -4,6 +4,7 @@ import com.chessd.chess.game.entity.Game;
 import com.chessd.chess.game.service.GameService;
 import com.chessd.chess.game.service.RandomUniqIdGenerator;
 import com.chessd.chess.figure.utils.Column;
+import com.chessd.chess.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Optional;
@@ -19,13 +21,32 @@ import java.util.Set;
 
 @Controller
 public class GameController {
+    private final UserService userService;
     GameService gameService;
     RandomUniqIdGenerator randomUniqIdGenerator;
 
     @Autowired
-    public GameController(GameService gameService, RandomUniqIdGenerator randomUniqIdGenerator) {
+    public GameController(GameService gameService, RandomUniqIdGenerator randomUniqIdGenerator, UserService userService) {
         this.gameService = gameService;
         this.randomUniqIdGenerator = randomUniqIdGenerator;
+        this.userService = userService;
+    }
+
+    @GetMapping("/play/test")
+    public String test(Model model, Principal principal){
+        Game g = new Game(randomUniqIdGenerator.generateUniqId());
+        g.setWhite(userService.findByUserName(principal.getName()));
+        gameService.save(g);
+        gameService.startGame(g);
+
+        model
+                .addAttribute("white", g.getWhite())
+                .addAttribute("black", g.getBlack())
+                .addAttribute("columns", Column.values())
+                .addAttribute("game", g)
+                .addAttribute("gameBoard", gameService.getBoardAsTable(g))
+                .addAttribute("gameService", gameService);
+        return "game/classic-board";
     }
 
     @GetMapping("/play/{gamId}")
