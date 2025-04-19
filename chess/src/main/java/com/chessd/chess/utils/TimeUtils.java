44fe3b0cb090b4c1fa34ba60d297b1b6c6db6f1e
DawidSzoka1 <oficer130@gmail.com>
2025-04-19 10:25:28 +0200

    changed time utils format to better suit website

--- chess/src/main/java/com/chessd/chess/utils/TimeUtils.java
+++ chess/src/main/java/com/chessd/chess/utils/TimeUtils.java
@@ -3,17 +3,25 @@ package com.chessd.chess.utils;
 import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
-import java.text.DecimalFormat;
+import java.time.Duration;
 
 @Component
 public class TimeUtils {
-    public double minutesBetween(Timestamp start, Timestamp end) {
-        return ((double) (end.getTime() - start.getTime())) / 1000 / 60;
+    private String calculateDiff(Timestamp start, Timestamp end){
+        Duration duration = Duration.ofMillis(end.getTime() - start.getTime());
+
+        long h = duration.toHours();
+        long m = duration.toMinutesPart();
+        long s = duration.toSecondsPart();
+
+        return String.format("%02d:%02d:%02d", h, m, s);
+    }
+
+    public String minutesBetween(Timestamp start, Timestamp end) {
+        return calculateDiff(start, end);
     }
 
     public String minutesBetween(Timestamp start) {
-        DecimalFormat format = new DecimalFormat("##.00");
-        double between = ((double) (new Timestamp(System.currentTimeMillis()).getTime() - start.getTime())) / 1000 / 60;
-        return (format.format(between)).replace(",", ":");
+        return calculateDiff(start, new Timestamp(System.currentTimeMillis()));
     }
 }
