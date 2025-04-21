b0ab96ce3159696b53c68be74bffaaaae7ba2a69
DawidSzoka1 <oficer130@gmail.com>
2025-04-21 13:35:43 +0200

    new profile html with css in for know plus changed game history result color

--- chess/src/main/java/com/chessd/chess/user/controller/ProfileController.java
+++ chess/src/main/java/com/chessd/chess/user/controller/ProfileController.java
@@ -1,8 +1,12 @@
 package com.chessd.chess.user.controller;
 
+import com.chessd.chess.game.entity.Game;
+import com.chessd.chess.game.service.GameService;
+import com.chessd.chess.game.utils.GameResult;
 import com.chessd.chess.user.entity.User;
 import com.chessd.chess.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,25 +16,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class ProfileController {
     private final UserService userService;
+    private final GameService gameService;
 
     @Autowired
-    public ProfileController(UserService userService) {
+    public ProfileController(UserService userService, GameService gameService) {
         this.userService = userService;
+        this.gameService = gameService;
     }
 
     @GetMapping("users/profile/{userName}")
     public String showUserProfile(@PathVariable String userName, Model model){
         User user = userService.findByUserName(userName);
         if(user == null){
-            throw new IllegalArgumentException("User with that id does not exist");
+            throw new IllegalArgumentException("User with that userName does not exist");
         }
-        String fullName = (user.getFirstName() == null ? "" : (user.getFirstName() + " ") ) +
-                (user.getLastName() == null ? "" : user.getLastName());
-        if(fullName.isEmpty()){
-            fullName = "brak danych";
-        }
-        model.addAttribute("fullName", fullName);
-        model.addAttribute("user", user);
+        Page<Game> games = gameService.getGamesByPlayer(0, 6, user);
+
+        model.addAttribute("user", user)
+                .addAttribute("won", gameService.countWonGames(user))
+                .addAttribute("lost", gameService.countLostGames(user))
+                .addAttribute("draw", gameService.countDrawGames(user))
+                .addAttribute("recentGames", games.getContent());
         return "users/profile";
     }
 }
