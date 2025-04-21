cf15af543ebcfaa309bdc264f84ff1694e026fcb
DawidSzoka1 <oficer130@gmail.com>
2025-04-21 11:51:09 +0200

    admin now also have user profile and game history plus lobby changed and some fixing errors in ranking with link to profile

--- chess/src/main/java/com/chessd/chess/game/controller/LobbyController.java
+++ chess/src/main/java/com/chessd/chess/game/controller/LobbyController.java
@@ -1,18 +1,41 @@
 package com.chessd.chess.game.controller;
+import com.chessd.chess.game.service.GameService;
+import com.chessd.chess.user.entity.User;
+import com.chessd.chess.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
 public class LobbyController {
+
+    private final UserService userService;
+    private final GameService gameService;
+
+    @Autowired
+    public LobbyController(UserService userService, GameService gameService) {
+        this.userService = userService;
+        this.gameService = gameService;
+    }
 
     @GetMapping("/play")
     public String showLobby(@AuthenticationPrincipal UserDetails userDetails, Model model){
 
         String playerName = userDetails == null ? "player" : userDetails.getUsername();
-        model.addAttribute("user", playerName);
+        User user = userService.findByUserName(playerName);
+        if(user == null){
+            return "game/lobby";
+        }
+
+        model.addAttribute("user", user)
+                .addAttribute("won", gameService.countWonGames(user))
+                .addAttribute("lost", gameService.countLostGames(user))
+                .addAttribute("draw", gameService.countDrawGames(user));
         return "game/lobby";
     }
 }
