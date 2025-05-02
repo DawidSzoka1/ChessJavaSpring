c9b9b5d0edee9af7ea8699744b46569df2091f91
DawidSzoka1 <oficer130@gmail.com>
2025-05-02 12:26:55 +0200

    change password html with new web user for changing password

---
+++ chess/src/main/java/com/chessd/chess/user/web/ChangePasswordUser.java
@@ -0,0 +1,31 @@
+package com.chessd.chess.user.web;
+
+import com.chessd.chess.customAnnotation.NoSpecialChars;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChangePasswordUser {
+
+    @NotNull(message = "Wymagane!")
+    @NoSpecialChars(message = "Obecne hasło nie może zawierac znaków specjalnych!")
+    @Size(min = 1, message = "Obecne hasło minimum długości jeden!")
+    private String currentPassword;
+
+    @NotNull(message = "Wymagane!")
+    @NoSpecialChars(message = "Nowe hasło nie może zawierac znaków specjalnych!")
+    @Size(min = 1, message = "Nowe hasło minimum długości jeden!")
+    private String newPassword;
+
+    @NotNull(message = "Wymagane!")
+    @NoSpecialChars(message = "Powtórzenie nowego hasła nie może zawierac znaków specjalnych!")
+    @Size(min = 1, message = "Powtórzenie nowego hasła minimum długości jeden!")
+    private String newPasswordRepeat;
+}
