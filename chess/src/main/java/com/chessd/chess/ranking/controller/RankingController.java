056d63af497f463d0943df2e644b0a6503a0c0c1
DawidSzoka1 <oficer130@gmail.com>
2025-05-06 19:58:01 +0200

    ranking controller html using data from database using th:each

--- chess/src/main/java/com/chessd/chess/ranking/controller/RankingController.java
+++ chess/src/main/java/com/chessd/chess/ranking/controller/RankingController.java
@@ -4,41 +4,35 @@ import com.chessd.chess.ranking.entity.Ranking;
 import com.chessd.chess.ranking.entity.RankingPosition;
 import com.chessd.chess.ranking.service.RankingPositionService;
 import com.chessd.chess.ranking.service.RankingService;
-import com.chessd.chess.user.entity.User;
-import com.chessd.chess.user.service.UserService;
-import com.chessd.chess.utils.PaginationUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("/ranking")
 public class RankingController {
 
-    private final UserService userService;
-    private final PaginationUtil paginationUtil;
     private final RankingPositionService rankingPositionService;
     private final RankingService rankingService;
 
     @Autowired
-    public RankingController(UserService userService, PaginationUtil paginationUtil, RankingPositionService rankingPositionService, RankingService rankingService) {
-        this.userService = userService;
-        this.paginationUtil = paginationUtil;
+    public RankingController(RankingPositionService rankingPositionService, RankingService rankingService) {
         this.rankingPositionService = rankingPositionService;
         this.rankingService = rankingService;
     }
 
-    @GetMapping("/all")
+    @GetMapping("")
     public String rankings(Model model) {
         List<Ranking> rankings = rankingService.findAll();
-        HashMap<Ranking, List<RankingPosition>> rankingListHashMap = new HashMap<>();
+        LinkedHashMap<Ranking, List<RankingPosition>> rankingListHashMap = new LinkedHashMap<>();
         for (Ranking r : rankings) {
             rankingListHashMap.put(r,
                     rankingPositionService
@@ -49,17 +43,22 @@ public class RankingController {
         return "ranking/all";
     }
 
-    @GetMapping("/classic")
-    public String classic(Model model,
-                          @RequestParam(
-                                  value = "pageNumber", defaultValue = "0", required = false
-                          ) int pageNumber,
-                          @RequestParam(
-                                  value = "pageSize", defaultValue = "10", required = false
-                          ) int pageSize
+    @GetMapping("/{rankingName}/all")
+    public String allByRankingName(Model model, @PathVariable String rankingName,
+                                   @RequestParam(
+                                           value = "pageNumber", defaultValue = "0", required = false
+                                   ) int pageNumber,
+                                   @RequestParam(
+                                           value = "pageSize", defaultValue = "10", required = false
+                                   ) int pageSize
     ) {
-        Page<User> page = userService.findAllByRanking(pageNumber, pageSize);
-        paginationUtil.setPagination(page, model);
-        return "users/rankings";
+        Ranking ranking = rankingService.findByName(rankingName);
+        if (ranking == null) {
+            return "error/404";
+        }
+        Page<RankingPosition> page = rankingPositionService.findAllByRanking(ranking, pageNumber, pageSize);
+        model.addAttribute("page", page)
+                .addAttribute("ranking", ranking);
+        return "ranking/specificRanking";
     }
 }
